d79ca4aeb784ef77048b5f85b4d9d9da3c920300
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-13 22:32:58 -0700

    Update Page.php
    fixed up the body tag code, updated to PHP 8.0 conventions.

--- Shared/Page.php
+++ Shared/Page.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * @author Alan T. Miller <alan@alanmiller.com>
+ * @copyright Copyright (C) 2023, Alan T Miller, All Rights Reserved.
+ *
+ * Class Shared_Page.
+ *
+ * This class provides an object-oriented way to generate HTML pages.
+ */
 class Shared_Page
 {
     private string $html;
@@ -24,45 +31,92 @@ class Shared_Page
         return $this;
     }
 
+    /**
+     * Set the title of the page.
+     *
+     * @param string $str
+     * @return $this
+     */
     public function setTitle(string $str): self
     {
         $this->title = $str;
         return $this;
     }
 
+    /**
+     * Set the suffix of the page title.
+     *
+     * @param string $suffix
+     * @return $this
+     */
     public function setTitleSuffix(string $suffix): self
     {
         $this->title_suffix = $suffix;
         return $this;
     }
 
-    public function setTitleSeparator(string $str): self
+    /**
+     * Set the separator character(s) for the page title.
+     *
+     * @param string $str
+     * @return $this
+     */
+    public function setTitleSeperator(string $str): self
     {
-        $this->title_separator = $str;
+        $this->title_seperator = $str;
         return $this;
     }
 
+    /**
+     * Set the id for the body tag.
+     *
+     * @param string $str
+     * @return $this
+     */
     public function setBodyId(string $str): self
     {
         $this->body_id = $str;
         return $this;
     }
 
+    /**
+     * Set the class for the body tag.
+     *
+     * @param string $str
+     * @return $this
+     */
     public function setBodyClass(string $str): self
     {
         $this->body_class = $str;
         return $this;
     }
 
+    /**
+     * Add or update a meta tag.
+     *
+     * @param string $key
+     * @param string $val
+     * @return $this
+     */
     public function setMetaData(string $key, string $val): self
     {
         $this->meta_tags[$key] = $val;
         return $this;
     }
 
+    /**
+     * Add a CSS stylesheet.
+     *
+     * @param string $url
+     * @param string $media
+     * @return $this
+     */
     public function addStyleSheet(string $url, string $media = 'all'): self
     {
-        $this->stylesheets[] = ['media' => $media, 'url' => $url];
+        $this->stylesheets[] = [
+            'media' => $media,
+            'url' => $url
+        ];
         return $this;
     }
 
@@ -77,15 +131,27 @@ class Shared_Page
         return $this;
     }
 
+    /**
+     * Set a comment that will be placed in the meta tags section.
+     *
+     * @param string $str
+     * @return $this
+     */
     public function setMetaComment(string $str): self
     {
         $this->meta_comment = $str;
         return $this;
     }
 
+    /**
+     * Add content to the body of the page.
+     *
+     * @param string $str
+     * @return $this
+     */
     public function addBodyContent(string $str): self
     {
-        $this->body = $str;
+        $this->body .= $str;
         return $this;
     }
 
@@ -154,17 +220,22 @@ class Shared_Page
         $this->append('<meta charset="UTF-8">');
         $this->append('<meta name="viewport" content="' . $this->viewport . '">');
 
-            // If author is set, append author meta tag
-    if ($this->author) {
-        $this->append('<meta name="author" content="' . $this->author . '">');
-    }
-
-            // Add JavaScript files to head if any
-    if (!empty($this->javascripts_head)) {
-        foreach ($this->javascripts_head as $javascript) {
-            $this->html .= "<script src=\"$javascript\"></script>\n";
+        // If author is set, append author meta tag
+        if ($this->author) {
+            $this->append('<meta name="author" content="' . $this->author . '">');
         }
-    }
+
+        // If meta comment is set, append meta comment
+        if ($this->meta_comment) {
+            $this->append('<!-- ' . $this->meta_comment . ' -->');
+        }
+
+        // Add JavaScript files to head if any
+        if (!empty($this->javascripts_head)) {
+            foreach ($this->javascripts_head as $javascript) {
+                $this->html .= "<script src=\"$javascript\"></script>\n";
+            }
+        }
 
         // Title
         $fullTitle = $this->title;
@@ -174,31 +245,40 @@ class Shared_Page
         $this->append('<title>' . ($fullTitle ?? $_SERVER["SCRIPT_NAME"]) . '</title>');
 
 
-    // Add meta tags
-    if (!empty($this->meta_tags)) {
-        foreach ($this->meta_tags as $name => $content) {
-            $this->html .= "<meta name=\"$name\" content=\"$content\">\n";
+        // Add meta tags
+        if (!empty($this->meta_tags)) {
+            foreach ($this->meta_tags as $name => $content) {
+                $this->html .= "<meta name=\"$name\" content=\"$content\">\n";
+            }
         }
-    }
 
-    // Add stylesheets
-    if (!empty($this->stylesheets)) {
-        foreach ($this->stylesheets as $stylesheet) {
-            $this->html .= "<link rel=\"stylesheet\" href=\"$stylesheet\">\n";
+        // Add stylesheets
+        if (!empty($this->stylesheets)) {
+            foreach ($this->stylesheets as $stylesheet) {
+                $this->html .= "<link rel=\"stylesheet\" href=\"$stylesheet\">\n";
+            }
         }
-    }
         $this->append('</head>');
-        $this->append('<body' . ($this->body_id ? ' id="' . $this->body_id . '"' : '') . ($this->body_class ? ' class="' . $this->body_class . '"' : '') . '>');
+
+        // Body tag with optional id and class attributes
+        $bodyAttr = [];
+        if ($this->body_id) {
+            $bodyAttr[] = 'id="' . $this->body_id . '"';
+        }
+        if ($this->body_class) {
+            $bodyAttr[] = 'class="' . $this->body_class . '"';
+        }
+        $this->append('<body' . (empty($bodyAttr) ? '' : ' ' . implode(' ', $bodyAttr)) . '>');
 
         // Body content
         $this->append($this->body ?? '');
 
-    // Add JavaScript files to body if any
-    if (!empty($this->javascripts_body)) {
-        foreach ($this->javascripts_body as $javascript) {
-            $this->html .= "<script src=\"$javascript\"></script>\n";
+        // Add JavaScript files to body if any
+        if (!empty($this->javascripts_body)) {
+            foreach ($this->javascripts_body as $javascript) {
+                $this->html .= "<script src=\"$javascript\"></script>\n";
+            }
         }
-    }
 
         $this->append('<!-- Created: ' . date('l jS \of F Y h:i:s A') . ' -->');
         $this->append('</body>');
